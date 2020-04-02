23f7685cac993c8a7e6972c76ce181acf019da54
Maira <mah-martins@live.com>
2020-04-02 19:03:33 -0300

    primos

--- primos.py
+++ primos.py
@@ -2,25 +2,31 @@ import os
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-def primo (n):
-    if n != 0 and n !=1:
-        if n>3:
-            for i in range(2,n):
-                if n %i==0:
-                    return False
-        return True
-    return
+@app.route('/')
+
+def programa ():
+    def primo (n):
+        if n != 0 and n !=1:
+            if n>3:
+                for i in range(2,n):
+                    if n %i==0:
+                        return False
+            return True
+        return
 
 
-i=1
-cont =0
-while True:
-    primo(i)
-    i+=1
-    if cont == 101:
-        break
-    if primo(i) == True:
-        cont+=1
-        print(i)
-        print(cont)
-   
+    i=1
+    cont =0
+    while True:
+        primo(i)
+        i+=1
+        if cont == 101:
+            break
+        if primo(i) == True:
+            cont+=1
+            return i
+            
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
